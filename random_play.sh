9cf307a6579b45b3c34960d0fe5ba7699234e815
base64g <base64@kmc.gr.jp>
2016-12-01 23:46:57 +0900

    sox使ったやり方に変更

--- random_play.sh
+++ random_play.sh
@@ -2,10 +2,11 @@
 i=0
 musics=(`ls PartMusic`)
 size=${#musics[*]}
+toplay=${musics[$RANDOM%$size]}
+cp "PartMusic/$toplay" output.wav
 while [ $i -le 100 ];
 do
     toplay=${musics[$RANDOM%$size]}
-    afplay "PartMusic/$toplay" &
-    sleep 11
+    sox "PartMusic/$toplay" output.wav output.wav
     i=$((i + 1));
 done
