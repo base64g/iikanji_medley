f83df14b449c3bef0f67971156732212935a2763
base64g <base64@kmc.gr.jp>
2016-12-09 13:08:25 +0900

    乱数を修正

--- random_play.sh
+++ random_play.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 i=0
+echo `od -A n -t u4 -N 4 /dev/urandom | sed 's/[^0-9]//g'`
 musics=(`ls PartMusic`)
 size=${#musics[*]}
-toplay=${musics[$RANDOM%$size]}
+toplay=${musics[`od -A n -t u4 -N 4 /dev/urandom | sed 's/[^0-9]//g'`%$size]}
 cp "PartMusic/$toplay" output.wav
-while [ $i -le 100 ];
+while [ $i -le 500 ];
 do
-    toplay=${musics[$RANDOM%$size]}
+    toplay=${musics[`od -A n -t u4 -N 4 /dev/urandom | sed 's/[^0-9]//g'`%$size]}
     echo $toplay
     echo "PartMusic/$toplay"
     sox output.wav PartMusic/${toplay} output2.wav
