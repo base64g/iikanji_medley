e4f519e8cfa0c535e677a40947caa88eb05f156c
base64g <base64@kmc.gr.jp>
2016-12-10 17:24:56 +0900

    norm付けた

--- random_play.sh
+++ random_play.sh
@@ -4,13 +4,15 @@ echo `od -A n -t u4 -N 4 /dev/urandom | sed 's/[^0-9]//g'`
 musics=(`ls PartMusic`)
 size=${#musics[*]}
 toplay=${musics[`od -A n -t u4 -N 4 /dev/urandom | sed 's/[^0-9]//g'`%$size]}
-cp "PartMusic/$toplay" output.wav
+sox "PartMusic/$toplay" output.wav norm
 while [ $i -le 500 ];
 do
     toplay=${musics[`od -A n -t u4 -N 4 /dev/urandom | sed 's/[^0-9]//g'`%$size]}
     echo $toplay
     echo "PartMusic/$toplay"
-    sox output.wav PartMusic/${toplay} output2.wav
+    sox PartMusic/${toplay} add.wav norm
+    sox output.wav add.wav output2.wav
     mv output2.wav output.wav
+    rm add.wav
     i=$((i + 1));
 done
